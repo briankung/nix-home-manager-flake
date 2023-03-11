1cd0bd03340f1586c217b5e3335081f61c10c5dc
Brian Kung <brianhingyenkung@gmail.com>
2023-03-11 09:26:10 -0600

    Initial commit
    Based off of https://juliu.is/tidying-your-home-with-nix/

---
+++ home.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }: {
+  home.username = "brian"; # REPLACE ME
+  home.homeDirectory = "/Users/brian"; # REPLACE
+  home.stateVersion = "22.11";
+  programs.home-manager.enable = true;
+}
