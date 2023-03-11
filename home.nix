435bcfa28e0d37848c158dbcf6310c8c8bff24b7
Brian Kung <brianhingyenkung@gmail.com>
2023-03-11 11:39:55 -0600

    Add git configuration

--- home.nix
+++ home.nix
@@ -3,4 +3,9 @@
   home.homeDirectory = "/Users/brian"; # REPLACE
   home.stateVersion = "22.11";
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    includes = [{ path = "~/.config/nixpkgs/gitconfig"; }];
+  };
 }
