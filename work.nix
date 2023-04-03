1e92148e79b4f5460c30579cb1e447dd45b88291
Brian Kung <github@briankung.xyz>
2023-04-03 09:58:59 -0500

    Shorten work stuff

--- work.nix
+++ work.nix
@@ -1,25 +1,10 @@
 { pkgs, ... }: {
   programs.git = {
-    enable = true;
-    userName = "Brian Kung";
-    userEmail = "brian.kung@backpacknetworks.com";
-    ignores = [ "*~" ".DS_Store" ".direnv" ".env" ".rgignore" ];
-    extraConfig = {
-      init = { defaultBranch = "main"; };
-      pull = { ff = "only"; };
-      push = { autoSetupRemote = true; };
-    };
-    delta = { enable = true; };
-    aliases = {
-      co = "checkout";
-      cleanbr =
-        "!git remote prune origin && git co master && git branch --merged | grep -v '*' | xargs -n 1 git branch -d && git co -";
-    };
+    userEmail = mkForce "brian.kung@backpacknetworks.com";
   };
 
   programs.zsh = {
-    enable = true;
-    initExtra =
+    initExtra = mkForce
       builtins.readFile ./zshrc + builtins.readFile ./wk-zshrc;
   };
 
