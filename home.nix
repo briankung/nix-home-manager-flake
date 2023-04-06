04b82afa18dfa63fc7dd52f7281c8517193257f4
Brian Kung <brian.kung@backpacknetworks.com>
2023-04-06 07:48:44 -0500

    Follow symlinks

--- home.nix
+++ home.nix
@@ -28,7 +28,7 @@
       # Directory navigation helpers
       dl = "cd ~/Downloads";
       play-nix = "cd ~/.config/nixpkgs";
-      wk = "cd $(fd . $HOME/Code --type=d | fzf)";
+      wk = "cd $(fd . $HOME/Code --type=d --follow | fzf)";
 
       # Git aliases
       gs = "git status";
