eb8f4697cd8ccc906e73a23e884b30638e25b41d
Brian Kung <github@briankung.xyz>
2023-04-05 22:10:22 -0500

    Nixfmt

--- wk.nix
+++ wk.nix
@@ -1,14 +1,9 @@
 { pkgs, lib, ... }: {
-  programs.git = lib.mkForce {
-    userEmail = "brian.kung@backpacknetworks.com";
-  };
+  programs.git = lib.mkForce { userEmail = "brian.kung@backpacknetworks.com"; };
 
   programs.zsh = lib.mkForce {
-    initExtra =
-      builtins.readFile ./zshrc + builtins.readFile ./wk-zshrc;
+    initExtra = builtins.readFile ./zshrc + builtins.readFile ./wk-zshrc;
   };
 
-  home.packages = [
-    pkgs.google-cloud-sdk
-  ];
+  home.packages = [ pkgs.google-cloud-sdk ];
 }
