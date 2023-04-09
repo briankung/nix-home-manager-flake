ffd612d79456290bfb82aa0c6a5c88d34eba3913
Brian Kung <github@briankung.xyz>
2023-04-09 10:35:13 -0500

    This'll probably work, let's commit it

--- wk.nix
+++ wk.nix
@@ -1,20 +1,5 @@
 { pkgs, lib, ... }: {
-  programs.git = lib.mkForce {
-    enable = true;
-    userName = "Brian Kung";
-    userEmail = "brian.kung@backpacknetworks.com";
-    ignores = [ "*~" ".DS_Store" ".direnv" ".env" ".rgignore" ];
-    extraConfig = {
-      init = { defaultBranch = "main"; };
-      pull = { ff = "only"; };
-      push = { autoSetupRemote = true; };
-    };
-    aliases = {
-      co = "checkout";
-      cleanbr =
-        "!git remote prune origin && git co master && git branch --merged | grep -v '*' | xargs -n 1 git branch -d && git co -";
-    };
-  };
+  programs.git.userEmail = lib.mkForce "brian.kung@backpacknetworks.com";
 
   programs.zsh.initExtra =
     lib.mkForce ((builtins.readFile ./zshrc) + (builtins.readFile ./wk-zshrc));
