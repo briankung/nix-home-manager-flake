fa3440e03ef7860cc5122d2d983bf2128ea155bc
Brian Kung <github@briankung.xyz>
2023-04-08 23:45:41 -0500

    Deduplicate wk.nix

--- wk.nix
+++ wk.nix
@@ -16,50 +16,8 @@
     };
   };
 
-  programs.zsh = lib.mkForce {
-    enable = true;
-    initExtra = builtins.readFile ./zshrc + builtins.readFile ./wk-zshrc;
-    shellAliases = {
-      # Directory navigation helpers
-      dl = "cd ~/Downloads";
-      play-nix = "cd ~/.config/nixpkgs";
-      wk = "cd $(fd . $HOME/Code --type=d | fzf)";
+  programs.zsh.initExtra =
+    lib.mkForce ((builtins.readFile ./zshrc) + (builtins.readFile ./wk-zshrc));
 
-      # Git aliases
-      gs = "git status";
-      gb = "git branch";
-      gd = "git diff";
-      gdc = "git diff --staged";
-      ga = "git add";
-      gp = "git push";
-      gap = "git add -p";
-      gl = "git log";
-      gc = "git commit";
-    };
-  };
-
-  home.packages = [
-    pkgs.coreutils
-    pkgs.dua
-    pkgs.fd
-    pkgs.fortune
-    pkgs.gnused
-    pkgs.htop
-    pkgs.libiconv
-    pkgs.nix-bash-completions
-    pkgs.nixfmt
-    pkgs.nodePackages.sql-formatter
-    pkgs.nodePackages_latest.mermaid-cli
-    pkgs.nodePackages_latest.typescript-language-server
-    pkgs.nodejs
-    pkgs.ponysay
-    pkgs.ripgrep
-    pkgs.rsync
-    pkgs.ruby_3_1
-    pkgs.tree
-    pkgs.wget
-    pkgs.zbar
-    # wk
-    pkgs.google-cloud-sdk
-  ];
+  home.packages = lib.mkAfter [ pkgs.google-cloud-sdk ];
 }
