5f102766361f94b3c672ae4e24b29803e564c76b
Brian Kung <github@briankung.xyz>
2023-10-03 09:04:17 -0500

    Adds reinstall script

---
+++ reinstall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -e
+
+# Uninstall nix
+echo "Uninstalling nix"
+/nix/nix-installer uninstall
+
+# Reinstall nix
+echo "Reinstalling nix"
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+
+# Set up profiles and gcroots directories
+echo "Setting up directories"
+sudo mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/$USER
+sudo chown -R $USER:nixbld /nix/var/nix/profiles/per-user/$USER
+
+# Reinstall home-manager
+echo "Reinstalling home-manager"
+nix run ".#homeConfigurations.$(scutil --get LocalHostName).activationPackage"
+
+echo "Run \`. ~/.zshrc\` to load your nix environment"
